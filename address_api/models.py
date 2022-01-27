ce2dc0eec80e772f3ce89569994c93eb04ef775e
Vishnu S Reddy <vishnusreddy007@gmail.com>
2022-01-27 10:55:51 +0530

    Created model for Address

--- address_api/models.py
+++ address_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+	name = models.CharField(max_length = 30)
+	phone_no = models.CharField(max_length=10)
+	door_no = models.CharField(max_length = 10)
+	street = models.CharField(max_length = 30)
+	locality = models.CharField(max_length = 30)
+	city = models.CharField(max_length = 20)
+	state = models.CharField(max_length = 20)
+	pincode = models.CharField(max_length = 6)
+
+	def __str__(self):
+		return (self.name + ", " + self.door_no + ", " + self.street + ", " + self.locality + ", " + 
+		self.city + ", " + self.state + ", " + self.pincode + ", Phone Number - " + self.phone_no)
