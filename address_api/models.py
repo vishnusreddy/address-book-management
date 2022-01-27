760e00eabb4755c02d31be90e3a89ab35076444a
Vishnu S Reddy <vishnusreddy007@gmail.com>
2022-01-27 17:23:03 +0530

    Added Phone Number is Unique Check

--- address_api/models.py
+++ address_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Address(models.Model):
 	name = models.CharField(max_length = 30)
-	phone_no = models.CharField(max_length=10)
+	phone_no = models.CharField(max_length=10, unique=True)
 	door_no = models.CharField(max_length = 10)
 	street = models.CharField(max_length = 50)
 	locality = models.CharField(max_length = 40)
--- address_api/urls.py
+++ address_api/urls.py
@@ -4,7 +4,6 @@ from address_api import views
 urlpatterns = [
     path("all-addresses/",views.ListAddressAPIView.as_view(),name="address_list"),
     path('all-addresses/<int:pincode>/', views.FilterAddressAPIView.as_view()),
-    #path("all-addresses/<int:pin>/",views.FilterAddressAPIView.as_view(),name="address_list_filter"),
     path("create/", views.CreateAddressAPIView.as_view(),name="address_create"),
     path("detail/<int:pk>/", views.DetailAddressAPIView.as_view(), name = "address_detail"),
     path("update/<int:pk>/",views.UpdateAddressAPIView.as_view(),name="update_address"),
