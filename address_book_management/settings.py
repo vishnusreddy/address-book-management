bb0a6db556efd234307159c925c66aafaf8cee66
Vishnu S Reddy <vishnusreddy007@gmail.com>
2022-01-27 10:49:21 +0530

    Connected to MySQL Database

--- address_book_management/settings.py
+++ address_book_management/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'address_book_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'address_book_management',
+        'USER': 'djangouser',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
